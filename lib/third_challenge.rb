5349e3f6c6deb058048d662819fe4c31318cd538
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-29 18:14:10 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,4 +1,4 @@
-def second_challenge
+def third_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {
@@ -16,13 +16,13 @@ def second_challenge
     :capulet => {
       :patriarch => {
         name: "Lord Capulet", age: "50"
-        };
+      },
       :matriarch => {
         name: "Lady Capulet", age: "51"
-        };
+      },
       :heroine => {
         name: "Juliet", age: "15", status: "alive"
-        };
+      },
       :heroine_friends => []
       #your key/value pairs here!
       }}
